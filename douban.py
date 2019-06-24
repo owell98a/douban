c5dc197a4e54345aadfc8ee865f70049824c3430
home <home@example.com>
2019-06-24 22:03:37 +0800

    添加延时，修改了初始化变量

--- douban.py
+++ douban.py
@@ -1,16 +1,18 @@
 from urllib import request
 from bs4 import BeautifulSoup
 import csv
+import time
 
 class movie_datas:
     def __init__(self,url):
         self.page_url = [url]
-        self.f_url = []
+
         self.grep = ['电影', '导演', '编剧', '主演', '类型', '语言', '上映日期', '片长', '又名']
         self.datas=[]
 
     # 把网页内容解析到BeautifulSoup可读
     def get_html(self,url):
+        time.sleep(2)
         req = request.urlopen(url)
         html = req.read().decode('utf-8')
         content = BeautifulSoup(html, 'html5lib')
@@ -18,6 +20,7 @@ class movie_datas:
 
     # 获取每页的url
     def get_page(self,url):
+        time.sleep(2)
         for i in range(1, 10):
             S = url + '?start=' + str(i * 25) + '&filter='
             self.page_url.append(S)
@@ -25,17 +28,20 @@ class movie_datas:
 
     # 获取每页中的电影跳转到主页面url, 返回list格式
     def get_movie(self,content):
+        time.sleep(2)
+        f_url=[]
         #content = self.get_html(url)
         movie_url = content.findAll('div', class_="item")
         #print(movie_url)
         for i in movie_url:
             film_url = i.a
             film_url = film_url.get('href')
-            self.f_url.append(film_url)
-        return self.f_url
+            f_url.append(film_url)
+        return f_url
 
     # 电影主界面解析,最终按照grep=['电影','导演','编剧','主演','类型','语言','上映日期','片长','又名'] 获取字段数据
     def movie_data(self,content):
+        time.sleep(2)
         #content = self.get_html(url)
         fname = content.find('div', id='content')
         fname = fname.find('span', property="v:itemreviewed")
@@ -64,11 +70,16 @@ class movie_datas:
         self.datas.append(self.grep)
         tmp=self.get_page(url)  #获取各页链接，返回list
         for i in tmp:
+            print(i)
             tmp1=self.get_html(i)  #返回html5lib格式文本
-            tmp2=self.get_movie(tmp1) #返回页面上的各电影链接 list
-            for i in tmp2:
+            #print (tmp1)
+
+            a=self.get_movie(tmp1) #返回页面上的各电影链接 list
+            print (a)
+            for i in a:
                 tmp3=self.get_html(i)
                 tmp4=self.movie_data(tmp3) #返回电影对应的信息 list
+                print(tmp4)
                 self.datas.append(tmp4)
         self.save_datas(self.datas)
 
